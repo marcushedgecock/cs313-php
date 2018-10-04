69ee9d9ea4119b6a92304b0bfa0d7f086913880b
marcus <hed15001@byui.edu>
2018-10-03 23:36:01 -0600

    making shopping cart

---
+++ web/browseItems.php
@@ -0,0 +1,21 @@
+<?php
+
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$garyBusey1 = $garyBusey2 = $garyBusey3 = $garyBusey4 = "";
+
+<img src="https://imgur.com/DdgevxH">
+<img src="https://imgur.com/DdgevxH">
+<img src="https://imgur.com/DdgevxH">
+<img src="https://imgur.com/DdgevxH">
+
+
+?>
+
+</body>
+</html>
