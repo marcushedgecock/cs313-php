8ced9003ce19c36f458e45ab5ae5940aaab4ee6f
marcus <hed15001@byui.edu>
2018-10-03 23:42:49 -0600

    adding more

--- web/browseItems.php
+++ web/browseItems.php
@@ -2,17 +2,17 @@
 
 session_start();
 ?>
-<!DOCTYPE html>
+<!doctype html>
 <html>
 <body>
 
 <?php
 $garyBusey1 = $garyBusey2 = $garyBusey3 = $garyBusey4 = "";
 
-<img src="https://imgur.com/DdgevxH">
-<img src="https://imgur.com/DdgevxH">
-<img src="https://imgur.com/DdgevxH">
-<img src="https://imgur.com/DdgevxH">
+<img src="https://imgur.com/DdgevxH.png">
+<img src="https://imgur.com/DdgevxH.png">
+<img src="https://imgur.com/DdgevxH.png">
+<img src="https://imgur.com/DdgevxH.png">
 
 
 ?>
